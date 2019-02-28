c7fa866b7a4b09a5f7305b93ff1cc9ff33fb68ca
Driversj <driversj@plu.edu>
2019-02-27 16:32:08 -0800

    Added Rectangle class and Rectangle Test

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Rectangle {
+
+    private int x;
+    private int y;
+
+    public Rectangle(int inX, int inY){
+        x = inX;
+        y = inY;
+    }
+    public int calculateArea(){
+        return x * y;
+    }
+
+    public int returnX(){
+        return x;
+    }
+
 }
---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+import static junit.framework.TestCase.assertEquals;
+
+
+public class RectangleTest{
+
+
+    @Test
+    public void CalculateAreaTest() {
+        Rectangle r = new Rectangle(3, 5);
+        assertEquals(15, r.calculateArea());
+    }
+
+    @Test
+    public void returnXTest(){
+        Rectangle r = new Rectangle(4, 9);
+        assertEquals(9, r.returnX());
+    }
+}
