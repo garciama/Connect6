e50306ca88c167495329a77cd6ce9b2d7b4079da
Michael Garcia <garciama@plu.edu>
2019-03-17 21:01:31 -0700

    Fixed an error that occurred within the checkDiagonalLeft method's first if statement.

--- src/main/java/core/game/Board.java
+++ src/main/java/core/game/Board.java
@@ -118,7 +118,12 @@ public class Board {
     }
 
     public boolean checkDiagonalLeft(int x, int y, Color c){
-        if (x < 5 || y < 5)
+        /* We need to talk about this coordinate system,
+         it was so weird to debug this to find that y can't be > 13.
+         We had a previous condition check for when y was less than
+         5, but that wasn't correct. I will explain this in our
+         next meeting. */
+        if (x < 5 || y > 13)
             return false;
         for (int i = 0; i < 6; i++){
             if(squaresOnBoard[x - i][y + i].getColor() != c)
