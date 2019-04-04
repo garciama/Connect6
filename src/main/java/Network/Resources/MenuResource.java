bcdfc0aa41958f96a1d1efeaa5ad9d8082397c71
Walker LaVoy <walkerlavoy@roam-218-112.plu.edu>
2019-04-04 16:43:50 -0700

    beginning to build network resources

--- src/main/java/Network/Resources/MenuResource.java
+++ src/main/java/Network/Resources/MenuResource.java
@@ -1,18 +1,29 @@
 package Network.Resources;
 
+import core.controller.GameController;
 import javax.ws.rs.GET;
-        import javax.ws.rs.POST;
-        import javax.ws.rs.Path;
-        import javax.ws.rs.Produces;
-        import javax.ws.rs.core.MediaType;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("menu")
 public class MenuResource {
-    
+
+    GameController controller = new GameController();
+
+    private void buildController() {
+        controller.registerNewPlayer("walker");
+        controller.registerNewPlayer("sam");
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String get() {
-        return "Hello there from your server!";
+        buildController();
+        int gameID = controller.newPublicGame("walker", "sam");
+        String board = controller.reportBoard(gameID);
+        return gameID + " " + board;
     }
 
     @POST
