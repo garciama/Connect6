4957182c1b2d0c5d1a7808db016b8a262d95885f
Sam <Driversj@plu.edu>
2019-04-04 17:01:38 -0700

    about to pull more of walker's code

--- src/main/java/Network/Resources/MenuResource.java
+++ src/main/java/Network/Resources/MenuResource.java
@@ -1,6 +1,8 @@
 package Network.Resources;
 
 import core.controller.GameController;
+import jdk.nashorn.internal.objects.annotations.Getter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,23 +14,40 @@ public class MenuResource {
 
     GameController controller = new GameController();
 
-    private void buildController() {
-        controller.registerNewPlayer("walker");
-        controller.registerNewPlayer("sam");
-    }
+//    private void buildController() {
+//        controller.registerNewPlayer("walker");
+//        controller.registerNewPlayer("sam");
+//    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() {
-        buildController();
-        int gameID = controller.newPublicGame("walker", "sam");
-        String board = controller.reportBoard(gameID);
-        return gameID + " " + board;
+    public String getMenu() {
+        return "Enter a number to select an option:\n1. Create a user\n2. Create a new game\n3. See games" +
+                " in progress\n4. Join a game\n5. See list of completed games\n6. See leaderboard\n";
+
+//        buildController();
+//        int gameID = controller.newPublicGame("walker", "sam");
+//        String board = controller.reportBoard(gameID);
+//        return gameID + " " + board;
+    }
+
+    @GET
+    @Path("menu/1")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getCreateUser(){
+        return "Enter your unique username (12 characters max, -1 to return to menu): ";
+
     }
 
     @POST
+    @Path("")
     @Produces(MediaType.TEXT_PLAIN)
     public String post() {
-        return "I received your POST";
+
+
+        //controller.registerNewPlayer();
+
+        return "User successfully created! Returning to menu...";
+
     }
 }
