0729f6a63e92477d62833cda06e083b909516ee3
ngesa@plu.edu <ngesa@plu.edu>
2019-04-11 17:07:56 -0700

    moved createGame to GameResource class

--- src/main/java/Network/Resources/GameResource.java
+++ src/main/java/Network/Resources/GameResource.java
@@ -1,4 +1,35 @@
 package Network.Resources;
 
+import Network.ModelGateway;
+import org.json.JSONObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class GameResource {
+    @PUT
+    @Path("createGame")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createGame(String players) {
+        JSONObject obj = new JSONObject(players);
+        String redPlayer = obj.getString("red");
+        String bluePlayer = obj.getString("blue");
+        Response res;
+
+        if(ModelGateway.getController().hasPlayerRegistered(redPlayer) && ModelGateway.getController().hasPlayerRegistered(bluePlayer)){
+            ModelGateway.getController().newPublicGame(redPlayer, bluePlayer);
+            String str = "game created";
+            res = Response.ok(str).build();
+        }
+        else{
+            String str1 = "players not found, try again or create players";
+            res = Response.status(404).entity(str1).build();
+        }
+        return res;
+    }
 }
--- src/main/java/Network/Resources/MenuResource.java
+++ src/main/java/Network/Resources/MenuResource.java
@@ -34,27 +34,7 @@ public class MenuResource {
         return res;
     }
 
-    @PUT
-    @Path("createGame")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response createGame(String players) {
-        JSONObject obj = new JSONObject(players);
-        String redPlayer = obj.getString("red");
-        String bluePlayer = obj.getString("blue");
-        Response res;
 
-        if(ModelGateway.getController().hasPlayerRegistered(redPlayer) && ModelGateway.getController().hasPlayerRegistered(bluePlayer)){
-            ModelGateway.getController().newPublicGame(redPlayer, bluePlayer);
-            String str = "game created";
-            res = Response.ok(str).build();
-        }
-        else{
-            String str1 = "players not found, try again or create players";
-            res = Response.status(404).entity(str1).build();
-        }
-        return res;
-    }
 
     @GET
     @Path("inProgress")
